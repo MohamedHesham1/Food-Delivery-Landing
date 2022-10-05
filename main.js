0a221b091c7f82d1c2df311cf14ebf6e845bc171
mohamed <mohamed.hesham.2049@gmail.com>
2022-10-05 23:51:48 +0200

    feat: :sparkles: add navbar to header

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import "./style.scss";
+const checkBox = document.querySelector(".nav__checkbox");
+const body = document.querySelector("body");
+const logo = document.querySelector(".logo");
+
+checkBox.addEventListener("click", () => {
+  console.log(`clicked `);
+  body.classList.toggle("scroll-hidden");
+  logo.classList.toggle("hidden");
+});
