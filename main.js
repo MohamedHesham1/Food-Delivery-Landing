87c79accb8c66b506dca4f8a837f4a1ea17f4720
mohamed <mohamed.hesham.2049@gmail.com>
2022-10-07 13:48:08 +0200

    feat: :sparkles: add most of hero section to main

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import "./style.scss";
 const checkBox = document.querySelector(".nav__checkbox");
 const body = document.querySelector("body");
 const logo = document.querySelector(".logo");
-
+const main = document.querySelector("main");
 checkBox.addEventListener("click", () => {
-  console.log(`clicked `);
   body.classList.toggle("scroll-hidden");
   logo.classList.toggle("hidden");
+  main.classList.toggle("hidden");
 });
