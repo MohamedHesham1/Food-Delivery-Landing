ab501d509783b01fce023608805c78a2a83aa7ef
mohamed <mohamed.hesham.2049@gmail.com>
2022-10-21 01:04:49 +0200

    feat: :sparkles: add scroll animation

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import "./style.scss";
+import sal from "sal.js";
+sal({ threshold: 0.5, once: false });
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 const checkBox = document.querySelector(".nav__checkbox");
